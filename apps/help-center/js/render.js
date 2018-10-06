2257b2a52eadf62e69df83ee9e85cb8e0d27d2cc
Kevinsal03 <kevinsal03@users.noreply.github.com>
2018-10-06 11:35:29 -0400

    help center styling and navigation, added check for sys files

--- apps/help-center/js/render.js
+++ apps/help-center/js/render.js
@@ -1,12 +1,23 @@
 const { app, BrowserWindow } = require('electron')
 var fs = require('fs')
+var $ = require("jquery")
+
+//default stylesheets
+$('head').append('<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css" />')
+$('head').append('<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>')
+
+
 
 console.log(fs.existsSync('/usr/share/bos/web-apps/'))
 
-var $ = require("jquery")
-
-$('head').append('<link rel="stylesheet" href="/usr/share/bos/web-apps/layout_1.css" />')
-//$('head').append('<link rel="stylesheet" href="layout.css" />') -- USING SYSTEM STORED CSS FILE ABOVE
-//TODO: CHECK IF SYSTEM STLYES EXIST, IF NOT, USE APP
-$('head').append('<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css" />')
-$('head').append('<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>')
+if(fs.existsSync('/usr/share/bos/web-apps/stylesheets') == true) {
+  //if system styles are found, use them
+  $('head').append('<link rel="stylesheet" href="/usr/share/bos/web-apps/layout_1.css" />')
+  console.log("using system stylesheets")
+} else {
+  //otherwise use local version
+  $('head').append('<link rel="stylesheet" href="layout.css" />')
+  console.log("ERROR: using local stylesheets")
+  alert("Error: Failed to load system stylesheets, using local backups. \nPlease make sure bos-local-styles package is installed. \n\nDEVELOPER NOTE: bls still in dev, error will appear on all systems!")
+  var stylsheetErr = 10;
+}
